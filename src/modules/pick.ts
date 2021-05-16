e7fa065d9c565d43668ac5810ea1a4f1115b48eb
Luca Barone <baro.luc@gmail.com>
2021-05-16 17:18:26 +0200

    :recycle: lift up UnknownObject type

--- src/modules/pick.ts
+++ src/modules/pick.ts
@@ -1,16 +1,17 @@
-type PickerKey = string | number | symbol
-type UObject = Record<string, unknown>
+import {UnknownObject} from './types/UnknownObject'
 
-type PickReturn<O extends UObject, K extends PickerKey> = Pick<
+type PickerKey = string | number | symbol
+
+type PickReturn<O extends UnknownObject, K extends PickerKey> = Pick<
   O,
   Exclude<keyof O, Exclude<keyof O, K>>
 >
 
-type PickerFunction<K extends PickerKey> = <O extends UObject>(
+type PickerFunction<K extends PickerKey> = <O extends UnknownObject>(
   obj: O,
 ) => PickReturn<O, K>
 
-export function pick<K extends PickerKey, O extends UObject>(
+export function pick<K extends PickerKey, O extends UnknownObject>(
   names: ReadonlyArray<K>,
   obj: O,
 ): PickReturn<O, K>
@@ -18,8 +19,8 @@ export function pick<K extends PickerKey>(
   names: ReadonlyArray<K>,
 ): PickerFunction<K>
 
-export function pick(names: ReadonlyArray<unknown>, obj?: UObject) {
-  const picker = (obj: UObject) =>
+export function pick(names: ReadonlyArray<unknown>, obj?: UnknownObject) {
+  const picker = (obj: UnknownObject) =>
     Object.keys(obj).reduce((acc, key) => {
       if (names.includes(key)) {
         return {...acc, [key]: obj[key]}
