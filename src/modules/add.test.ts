44926e65e7562ae789ccbdc7b2af25a49981f0e8
Luca Barone <baro.luc@gmail.com>
2021-05-30 01:43:52 +0200

    :sparkles: support partial application for add function

--- src/modules/add.test.ts
+++ src/modules/add.test.ts
@@ -1,6 +1,6 @@
 import {add} from './add'
 
-test('should work properly', () => {
+test('works properly', () => {
   expect(add(0, 0)).toBe(0)
 
   expect(add(1, 1)).toBe(2)
@@ -10,4 +10,16 @@ test('should work properly', () => {
 
   expect(add(3, 5)).toBe(8)
   expect(add(5, 3)).toBe(8)
+})
+
+test('supports partial application', () => {
+  expect(add(0)(0)).toBe(0)
+
+  expect(add(1)(1)).toBe(2)
+
+  expect(add(0)(10)).toBe(10)
+  expect(add(10)(0)).toBe(10)
+
+  expect(add(3)(5)).toBe(8)
+  expect(add(5)(3)).toBe(8)
 })
--- src/modules/add.ts
+++ src/modules/add.ts
@@ -1,1 +1,7 @@
-export const add = (a: number, b: number) => a + b
+export function add(a: number, b: number): number
+export function add(a: number): (b: number) => number
+export function add(a: number, b?: number) {
+  const adder = (b: number) => a + b
+
+  return b != null ? adder(b) : adder
+}
